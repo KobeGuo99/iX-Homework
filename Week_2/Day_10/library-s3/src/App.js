c9fc151fd87b5b25d92d086c33334c6d69abadce
Kobe Guo <kobeguo02@gmail.com>
2023-07-22 18:29:14 +0200

    day 10 hw done

--- Week_2/Day_10/library-s3/src/App.js
+++ Week_2/Day_10/library-s3/src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BookPage from "./components/book/BookPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/auth/LoginPage";
+import RegisterPage from "./components/auth/RegisterPage";
+import Navbar from "./components/common/Navbar";
+import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase";
 
 function App() {
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log(user);
+    });
+  }, []);
 
   return (
-    <div>
-      <BookPage />
-    </div>
+    <BrowserRouter>
+      <Navbar user={user}/>
+      <Routes>
+        <Route path="/" element={<BookPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
